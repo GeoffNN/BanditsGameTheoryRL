b669aef1d693741cb4108fcd45272bc0ec3ae96f
GeoffNN <geoffrey.negiar@polytechnique.edu>
2016-12-02 22:35:17 +0100

    Policy with parameters implemented

--- soda_strategy.py
+++ soda_strategy.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def soda_strategy_discount(n_energy, n_nosugar):
     if n_energy <= n_nosugar:
         discount = -0.2;
@@ -10,3 +13,18 @@ def soda_strategy_discount(n_energy, n_nosugar):
 def soda_strategy_nodiscount(n_energy, n_nosugar):
     discount = 0.0;
     return discount
+
+
+def soda_strategy_param(n_energy, n_nosugar, T, V):
+    assert len(T) == len(V)
+    diff = abs(n_energy - n_nosugar)
+    sg = np.sign(n_energy - n_nosugar)
+    if diff < T[0]:
+        return 0
+    if T[0] < diff <= T[1]:
+        return sg * V[0]
+    if T[1] < diff <= T[2]:
+        return sg * V[1]
+    if T[2] < diff:
+        return sg * V[2]
+    return 0
--- test_simulator.py
+++ test_simulator.py
@@ -1,5 +1,5 @@
 from simulator import Simulator
-from soda_strategy import soda_strategy_discount
+from soda_strategy import soda_strategy_discount, soda_strategy_param, soda_strategy_nodiscount
 import numpy as np
 
 # Test simulator
@@ -9,13 +9,17 @@ R = 500  # nb of restocks to estimate the expectation
 tot_rewards = np.zeros(R)
 tot_sodas = np.zeros(R)
 
+T = [2, 5, 10]
+V = [.1, .3, .5]
+# policy = lambda n_e, n_n: soda_strategy_param(n_e, n_n, T, V)
+# policy = soda_strategy_nodiscount
 policy = soda_strategy_discount
 
 for r in range(R):
     s.reset()  # refill
     rew, n_energy, n_nosugar = s.simulate(0)
     reward = rew
-    t = 1
+    t = 0
     while n_energy > 0 and n_nosugar > 0:
         # no re-fill is needed
         discount = policy(n_energy, n_nosugar)
@@ -23,4 +27,7 @@ for r in range(R):
         reward = reward + rew
         t += 1
     tot_rewards[r] = reward  # total reward obtained
-    tot_sodas[r] = t  # number of sodas solde
+    tot_sodas[r] = t  # number of sodas sold
+
+print(tot_sodas.mean())
+print(tot_rewards.mean())
